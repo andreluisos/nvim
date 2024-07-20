c4b659bb5b998b2fb6014fbb944c52d9958ddcdc
= <=>
2024-07-20 10:42:48 -0300

    Add DAP keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,9 @@
 -- Add any additional keymaps here
 vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
 vim.keymap.set('n', 's', ':HopWord<CR>', { desc = 'Hop to word' })
+
+-- [[ DAP ]]
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
