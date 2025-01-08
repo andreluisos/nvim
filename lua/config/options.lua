40b7a0fafa5a4ff0888ed32453169240d0c73775
André Luís <andreluisos@msn.com>
2025-01-08 12:01:37 -0300

    Update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
 vim.opt.mouse = ""
 vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
 vim.o.expandtab = false -- Pressing the TAB key will insert spaces instead of a TAB character
--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -85,4 +85,23 @@ return {
 			},
 		},
 	},
+	{
+		"saghen/blink.cmp",
+		optional = true,
+		dependencies = { "giuxtaposition/blink-cmp-copilot" },
+		opts = {
+			sources = {
+				default = { "copilot" },
+				providers = {
+					copilot = {
+						name = "copilot",
+						module = "blink-cmp-copilot",
+						kind = "Copilot",
+						score_offset = 0,
+						async = true,
+					},
+				},
+			},
+		},
+	},
 }
