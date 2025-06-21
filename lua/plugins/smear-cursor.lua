0147496e6204ed27e50f8daf59c302b904e948ad
André Luís <zandreluisos@gmail.com>
2025-06-21 12:08:28 -0300

    Add smear-cursor

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,25 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  event = "VeryLazy",
+  cond = vim.g.neovide == nil,
+  opts = {
+    cursor_color = "#ff8800",
+    stiffness = 0.3,
+    trailing_stiffness = 0.1,
+    damping = 0.5,
+    trailing_exponent = 5,
+    never_draw_over_target = true,
+    hide_target_hack = true,
+    gamma = 1,
+  },
+  specs = {
+    -- disable mini.animate cursor
+    {
+      "echasnovski/mini.animate",
+      optional = true,
+      opts = {
+        cursor = { enable = false },
+      },
+    },
+  },
+}
