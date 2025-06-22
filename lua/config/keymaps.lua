5e5cf66851748a9d41f9bf8a5367873298d25828
André Luís <zandreluisos@gmail.com>
2025-06-22 12:47:48 -0300

    Add dap keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- [[ DAP ]]
+vim.keymap.set('n', '<F5>', function()
+  require('dap').continue()
+end)
+vim.keymap.set('n', '<F9>', function()
+  require('dap').step_over()
+end)
+vim.keymap.set('n', '<F8>', function()
+  require('dap').step_into()
+end)
+vim.keymap.set('n', '<F7>', function()
+  require('dap').step_out()
+end)
