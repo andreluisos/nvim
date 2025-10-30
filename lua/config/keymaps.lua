16a790933667f4942a3bd61ce515cbd7a5a58c73
André Luís <zandreluisos@gmail.com>
2025-10-30 15:07:40 -0300

    Add keymap to copy file's absolute path

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,18 +2,34 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+vim.keymap.set("n", "<leader>fC", function()
+  -- %:p is the Neovim command-line modifier to get the full absolute path of the current file.
+  local absolute_path = vim.fn.expand("%:p")
+
+  -- vim.fn.setreg('+') sets the contents of the system clipboard (+)
+  vim.fn.setreg("+", absolute_path)
+
+  -- Provide a notification to the user that the operation was successful
+  vim.notify("Copied absolute path to clipboard: " .. absolute_path, vim.log.levels.INFO, {
+    title = "File Path Copied",
+  })
+end, {
+  noremap = true,
+  silent = true,
+  desc = "Copy absolute file path to clipboard",
+})
 
 -- [[ DAP ]]
-vim.keymap.set('n', '<F5>', function()
-  require('dap').continue()
+vim.keymap.set("n", "<F5>", function()
+  require("dap").continue()
 end)
-vim.keymap.set('n', '<F9>', function()
-  require('dap').step_over()
+vim.keymap.set("n", "<F9>", function()
+  require("dap").step_over()
 end)
-vim.keymap.set('n', '<F8>', function()
-  require('dap').step_into()
+vim.keymap.set("n", "<F8>", function()
+  require("dap").step_into()
 end)
-vim.keymap.set('n', '<F7>', function()
-  require('dap').step_out()
+vim.keymap.set("n", "<F7>", function()
+  require("dap").step_out()
 end)
