ddc25cf9c67e5e371e3544d4c4840a91514db73b
André Luís <zandreluisos@gmail.com>
2025-10-31 17:41:29 -0300

    Fix Rust identation

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,8 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.mouse = "a"       -- Enable mouse in all modes (optional, but scroll needs this)
+vim.opt.mouse = "a" -- Enable mouse in all modes (optional, but scroll needs this)
 vim.opt.mousefocus = true -- Optional: Allow window focus changes via mouse
+vim.g.rust_recommended_style = 0
 
 -- Disable mouse clicks (left, right, middle click) but keep scrolling
 vim.cmd([[
