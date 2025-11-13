b0211f6a728db253201dc0ee2014279306f084a2
André Luís <zandreluisos@gmail.com>
2025-11-13 18:42:46 -0300

    Add neovide config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,16 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.mouse = "a" -- Enable mouse in all modes (optional, but scroll needs this)
-vim.opt.mousefocus = true -- Optional: Allow window focus changes via mouse
+vim.o.mouse = "a" -- Enable mouse in all modes (optional, but scroll needs this)
+vim.o.mousefocus = true -- Optional: Allow window focus changes via mouse
+vim.o.clipboard = "unnamedplus"
+vim.o.linespace = 1
 vim.g.rust_recommended_style = 0
+vim.g.neovide_fullscreen = true
+vim.g.neovide_padding_top = 0
+vim.g.neovide_padding_bottom = 0
+vim.g.neovide_padding_left = 0
+vim.g.neovide_padding_right = 0
 
 -- Disable mouse clicks (left, right, middle click) but keep scrolling
 vim.cmd([[
