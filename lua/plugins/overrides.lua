3e6537079a7ca1ca938be08b415622a2d63bf8e2
André Luís <zandreluisos@gmail.com>
2025-11-20 17:54:37 -0300

    Disable animations

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -5,38 +5,18 @@ else
     { "folke/snacks.nvim", opts = { dashboard = { enabled = false } } },
     {
       "nvim-mini/mini.animate",
-      enabled = not vim.g.neovide,
+      enabled = false,
     },
 
     {
       "karb94/neoscroll.nvim",
-      enabled = not vim.g.neovide,
+      enabled = false,
     },
 
     {
       "sphamba/smear-cursor.nvim",
       event = "VeryLazy",
-      cond = vim.g.neovide == nil,
-      opts = {
-        cursor_color = "#AFD802",
-        stiffness = 0.3,
-        trailing_stiffness = 0.1,
-        damping = 0.5,
-        trailing_exponent = 5,
-        never_draw_over_target = true,
-        hide_target_hack = true,
-        gamma = 1,
-      },
-      specs = {
-        -- disable mini.animate cursor
-        {
-          "nvim-mini/mini.animate",
-          optional = true,
-          opts = {
-            cursor = { enable = false },
-          },
-        },
-      },
+      enabled = false
     },
 
     {
